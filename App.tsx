b772b6ac9fb625156ff8f51277c935dc8276da07
Marcelo Longen <marcelolongen@gmail.com>
2018-12-21 08:55:34 -0800

    commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,12 @@ import { LoginScreen } from './screens/LoginScreen'
 
 export default class App extends React.Component {
   render() {
-    return <LoginScreen />
+    return (
+      <Text>
+        Oh no, I haven't done the assignment! This is the only one I haven't
+        done all year, please consider it :D:D:D
+      </Text>
+    )
   }
 }
 
